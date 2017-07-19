bb1c61c31d133f3c2899a83e8ff9ac5b40ad9368
lyx <26095970@qq.com>
2017-07-19 18:35:10 +0800

    no message

--- zdhcs.rb
+++ zdhcs.rb
@@ -5,7 +5,10 @@ require 'json'
 require 'rexml/document'
 require 'win32ole'
 require 'watir'
- def access_page(web_name,clear_cache="是")
+require 'find'
+
+#访问一次网站并记录访问时间
+def access_page(web_name,clear_cache="是")
       control = WIN32OLE.new("HttpWatch.Controller")
       web_name_ie = Watir::Browser.new
       plugin = control.IE.Attach(web_name_ie.ie)
@@ -26,24 +29,42 @@ require 'watir'
         plugin.Stop()
       end
       ip = plugin.log.Entries.Item(0).ServerIP
-      puts "访问网页的IP是#{ip.chomp}"
+      puts "本次访问网页的IP是#{ip.chomp}"
       code = plugin.Log.Entries.Item(0).StatusCode
-      puts code
+      #puts code
       if code.to_s == "200"
          time_elapsed = plugin.Log.Entries.Summary.Time
-         puts  "访问网页花费的时间 = #{time_elapsed}"
+         puts  "本次访问网页花费的时间 = #{time_elapsed}"
       else
-        puts "访问网页失败"
+        puts "本次访问网页失败"
         @fail_times += 1
       end
       close_browser(web_name_ie)
+      @ZX_times += 1
       return time_elapsed
-    end
-
+ end
+#循环访问网站并统计结果
+def access_times(web_name,times)
+  @fail_times = 0
+  @ZX_times = 0
+  time_array = []
+  times.to_i.times do
+    time_array << access_page(web_name)
+    time_eclipsed = calculate_average( time_array )
+    access_times = @ZX_times.to_i - @fail_times.to_i
+    puts "#################################"
+    puts "#访问的网站是#{web_name.chomp}"
+    puts "#总共尝试访问#{@ZX_times}次,访问失败#{@fail_times}次    #"
+    puts "#成功访问网页#{access_times}次的平均值是#{time_eclipsed.round(3)} #"
+    puts "#################################"
+    sleep 2
+  end
+end
+#关闭IE浏览器
 def close_browser(web_page_ie)
       web_page_ie.close
 end
-
+#算出成功访问网站延时的平均值
 def calculate_average(param_array)
       return 0 if param_array.nil?
       return 0 if param_array.size <= 0
@@ -74,22 +95,13 @@ def calculate_average(param_array)
       return avg
 end
 
-puts "#####################"
-puts "#      wellcome     #"
-puts "#####################"
-puts "请输入网址"
-web_name=gets
+puts "#################################"
+puts "#            wellcome           #"
+puts "#################################"
 puts "请输入执行次数"
 times =gets
-@fail_times = 0
-puts "要#{times.chomp}次访问的网页是#{web_name}"
-time_array = []
-times.to_i.times do
-  time_array << access_page(web_name)
-  sleep 2
+File.open("C:\\name.txt") do |io|
+ io.each_line do |line|
+   access_times(line,times)
+ end
 end
-time_eclipsed = calculate_average( time_array )
-access_times = times.to_i - @fail_times.to_i
-puts  "访问网页#{access_times}次的平均值是#{time_eclipsed}"
-puts "网页访问失败#{@fail_times}次"
-
